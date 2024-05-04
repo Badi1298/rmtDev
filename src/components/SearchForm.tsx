d78e168191bf246441b58d8197353d38c08fdb33
Badi1298 <serbandavid83@yahoo.com>
2024-05-04 21:53:31 +0300

    app setup + begin search functionality

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,16 +1,38 @@
-export default function SearchForm() {
-  return (
-    <form action="#" className="search">
-      <button type="submit">
-        <i className="fa-solid fa-magnifying-glass"></i>
-      </button>
+import React, { useEffect, useState } from "react";
 
-      <input
-        spellCheck="false"
-        type="text"
-        required
-        placeholder="Find remote developer jobs..."
-      />
-    </form>
-  );
+const URL = "https://bytegrad.com/course-assets/projects/rmtdev/api/data";
+
+export default function SearchForm() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    useEffect(() => {
+        if (!searchQuery) return;
+
+        fetchResults(searchQuery);
+    }, [searchQuery]);
+
+    const fetchResults = (query: string): void => {
+        console.log(query);
+    };
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(e.target.value);
+    };
+
+    return (
+        <form className="search" onSubmit={(e) => e.preventDefault()}>
+            <button type="submit">
+                <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+
+            <input
+                value={searchQuery}
+                spellCheck="false"
+                type="text"
+                required
+                placeholder="Find remote developer jobs..."
+                onChange={handleSearch}
+            />
+        </form>
+    );
 }
